5fcacf85bbfe695fadf8e2d5d4ed0bdc90e00383
Abdul Rehman <a.rehman553@gmail.com>
2022-04-30 12:35:28 +0500

    Add files via upload

---
+++ js/jquery.main.js
@@ -0,0 +1,9 @@
+//langSwitch init
+$(document).ready(function () {
+    langSwitch();
+}); 
+
+//parallax init
+$(document).ready(function () {
+    parallaxEffect('.parallax-gallery-images', '.column', 'top center', 'bottom+=15% center', 'vertical', 'all'); 
+}); 
